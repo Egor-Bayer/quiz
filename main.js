ea3333316ccfdf2254b84b3fa6d8f6c4980a4615
Kpa6uLLLe <103505023+Kpa6uLLLe@users.noreply.github.com>
2023-11-22 01:57:51 +0600

    Info Tab
    табличка с инфой перед началом квиза

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+const infoCon = document.querySelector(".info_con")
 const formCon = document.querySelector(".form_con")
-const formConHtmlForm = formCon.innerHTML
 const form = document.getElementById("form")
-const formHtml = form.innerHTML
-const resultCon = document.querySelector(".result_con")
-const resultHtml = resultCon.innerHTML
 const quizBtn = document.querySelector(".start")
+const closeBtns = document.querySelectorAll(".close_btn")
+const proceedBtn = document.querySelector(".proceed_btn")
 const options = document.querySelectorAll(".option")
 const counter = document.querySelector(".count")
 const max = document.querySelector(".max_count")
 const question = document.querySelector(".question")
-
+const resultCon = document.querySelector(".result_con")
+const resultHtml = resultCon.innerHTML
 const questions = [
   "1+1?",
   "10+14?",
@@ -25,100 +25,99 @@ const answers = [
     ["11","21","31","41","51"],
     ["15","24","33","42","51"]
 ]
-let quizTitle = "Quiz guide"
-let quizText = "quiz text"
-
-
-let formConDefHtml = `<div class="quiz_title">${quizTitle}</div><div class="quiz_text">${quizText}</div>`
-formCon.innerHTML = formConDefHtml
-const buttons = document.createElement("div")
-const nextBtn = document.createElement("button")
-const closeBtn = document.createElement("button")
-nextBtn.setAttribute("id", "next_btn")
-nextBtn.classList.add("smaller_btn")
-nextBtn.innerHTML = "next"
-closeBtn.setAttribute("id", "close_btn")
-closeBtn.classList.add("smaller_btn")
-closeBtn.innerHTML = "close"
-buttons.appendChild(nextBtn)
-buttons.appendChild(closeBtn)
-formCon.appendChild(buttons)
-
 let givenAnswers = []
 let count = 0
 const MAXCOUNT = questions.length > options.length ? options.length : questions.length
-function quizInit(){
-  formCon.innerHTML = formConHtmlForm
-  newQuestion()
-  updateCount(count)
-  max.innerHTML = MAXCOUNT
+newQuestion()
+updateCount(count)
+max.innerHTML = MAXCOUNT
+  function reset() {
+    count = 0;
+    givenAnswers = []
+    newQuestion();
 }
-function reset() {
-  count = 0;
-  givenAnswers = []
-  newQuestion();
-}
-function showForm() {
-  formCon.style.top = "0px";
-}
-
-function hiddenForm() {
+function hideAll(){
+  infoCon.style.top = null;
   formCon.style.top = null;
+  resultCon.style.top = null;
   reset();
 }
-function showResult(){
-  hiddenForm();
-  resultCon.style.top = "0px"
-}
-function hideResult(){
-  hiddenForm();
-  resultCon.style.top = null;
-  setTimeout(() => {
-    resultCon.innerHTML = resultHtml
-  }, 500) 
-}
-function updateCount(){
-  counter.innerHTML = count
-  return
-}
-function newQuestion() {
-  count++
-  if(count>MAXCOUNT){
-    console.log(givenAnswers)
-      let html = ""
-      let sum = 0;
-      givenAnswers.forEach((i) =>{
-        sum += i
-      })
-      html += `<div class="result_title"><div>You've got ${sum} questions right</div>`
-      console.log(html)
-      let i = 1
-      givenAnswers.forEach((answer) => {
-        let str = answer ? "+" : "-"
-        html += `<div>${i}) ${str} <div/>`
-        i++
-      })
-      html += '<div class="click_tip">Click anywhere to close</div></div>'
-      resultCon.innerHTML = html
-      showResult();
-      return
+  function showInfo() {
+    infoCon.style.top = "0px"
   }
-  question.innerHTML = questions[count - 1]
-  for(let i = 1; i<=MAXCOUNT; i++){
-      let id = "o" + i
-      document.querySelector(`label[for=${id}]`).innerHTML = answers[count - 1][i-1]
+  function hideInfo(){
+    infoCon.style.top = null;
   }
-  updateCount();
-  return
-}
-function answerGiven(optionNumber, event){
-  console.log(optionNumber, event)
-  givenAnswers.push(correctAnswers[count - 1] == optionNumber)
-  newQuestion();
-}
-quizBtn.addEventListener("click", showForm);
-closeBtn.addEventListener("click", hiddenForm);
-nextBtn.addEventListener("click", quizInit)
+  function showForm() {
+    infoCon.style.top = null;
+    formCon.style.top = "0px";
+  }
+  
+  function hideForm() {
+    formCon.style.top = null;
+    reset();
+  }
+  function showResult(){
+    hideForm();
+    resultCon.style.top = "0px"
+  }
+  function hideResult(){
+    hideForm();
+    resultCon.style.top = null;
+    setTimeout(() => {
+      resultCon.innerHTML = resultHtml
+    }, 500) 
+  }
+  function updateCount(){
+    counter.innerHTML = count
+    return
+  }
+  function clearRadio(){
+    options.forEach((option) => {
+      option.checked = false
+    })
+  }
+  function newQuestion() {
+    count++
+    if(count>MAXCOUNT){
+      console.log(givenAnswers)
+        let html = ""
+        let sum = 0;
+        givenAnswers.forEach((i) =>{
+          sum += i
+        })
+        html += `<div class="result_title"><div>You've got ${sum} questions right<div/>`
+        console.log(html)
+        let i = 1
+        givenAnswers.forEach((answer) => {
+          let str = answer ? "+" : "-"
+          html += `<div>${i}) ${str} <div/>`
+          i++
+        })
+        html += '<div class="click_tip">Click anywhere to close<div/><div/>'
+        resultCon.innerHTML = html
+        showResult();
+        return
+    }
+    question.innerHTML = questions[count - 1]
+    for(let i = 1; i<=MAXCOUNT; i++){
+        let id = "o" + i
+        document.querySelector(`label[for=${id}]`).innerHTML = answers[count - 1][i-1]
+    }
+    updateCount();
+    return
+  }
+  function answerGiven(optionNumber, event){
+    console.log(optionNumber, event)
+    givenAnswers.push(correctAnswers[count - 1] == optionNumber)
+    newQuestion();
+    clearRadio();
+  }
+quizBtn.addEventListener("click", showInfo);
+proceedBtn.addEventListener("click", showForm)
+closeBtns.forEach((closeBtn) => {
+  closeBtn.addEventListener("click", hideAll);
+})
 resultCon.addEventListener("click", hideResult)
 for(let i = 0; i<options.length; i++){
   options[i].addEventListener("click", answerGiven.bind(null, i))
